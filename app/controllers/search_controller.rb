fc55f6bf925d1c43402d8f91e5bada7fd70f53c2
Eliot Swank <eliotswank@gmail.com>
2018-08-29 09:59:43 -0600

    adds api logic to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,15 @@
 class SearchController < ApplicationController
   def index
-    
+    zip = params[:zip]
+    @conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
+      faraday.headers["X-API-KEY"] = "9QJhTjDEGmGnzvFWWEF4jfeMXC57X9c6K6l3gcvu"
+      faraday.adapter Faraday.default_adapter
+    end
+
+require 'pry'; binding.pry
+
+    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?zip=#{zip}")
+
+    @stations = JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
