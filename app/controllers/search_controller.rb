28e28cd8bbe8f1651852607fb2b5fde609096958
Eliot Swank <eliotswank@gmail.com>
2018-08-29 11:04:01 -0600

    moves api key yml

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,9 @@ class SearchController < ApplicationController
   def index
     zip = params[:q]
     @conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
-      faraday.headers["X-API-KEY"] = "9QJhTjDEGmGnzvFWWEF4jfeMXC57X9c6K6l3gcvu"
+      faraday.headers["X-API-KEY"] = ENV["nrel_gov_key"]
       faraday.adapter Faraday.default_adapter
     end
-
     response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{zip}&fuel_type=ELEC,LPG")
 
     results = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
