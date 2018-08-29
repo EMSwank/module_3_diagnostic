147e1337fd89cfc89c33963575daed2e413f2f6e
Eliot Swank <eliotswank@gmail.com>
2018-08-29 10:12:14 -0600

    can get JSON now into the conttroller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,12 @@
 class SearchController < ApplicationController
   def index
-    zip = params[:zip]
+    zip = params[:q]
     @conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
       faraday.headers["X-API-KEY"] = "9QJhTjDEGmGnzvFWWEF4jfeMXC57X9c6K6l3gcvu"
       faraday.adapter Faraday.default_adapter
     end
-
-require 'pry'; binding.pry
-
-    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?zip=#{zip}")
+    
+    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{zip}")
 
     @stations = JSON.parse(response.body, symbolize_names: true)[:results]
   end
